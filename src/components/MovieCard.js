56ac29cf5aa8441a02b18e5758217a08aa018b0c
ygarciaglez <ygarciaglez1995@gmail.com>
2026-08-07 02:07:22 -0600

    feat(img): adding handle error function to set a default image source

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,17 @@ import React from "react";
 import "../styles.css";
 
 export default function MovieCard({ movie }) {
+  const handleError = (e) => {
+    e.target.src = "images/default.jpg";
+  };
+
   return (
     <div key={movie.id} className="movie-card">
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
