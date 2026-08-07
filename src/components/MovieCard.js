b7429cfadd01a58ec9df6a217d6dc9627391f64b
ygarciaglez <ygarciaglez1995@gmail.com>
2026-08-07 02:18:18 -0600

    style(components):  add dynamic color classname for movies rating

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,6 +6,12 @@ export default function MovieCard({ movie }) {
     e.target.src = "images/default.jpg";
   };
 
+  const getRatingClass = (rating) => {
+    if (rating >= 8) return "rating-good";
+    if (rating >= 5 && rating < 8) return "rating-ok";
+    return "rating-bad";
+  };
+
   return (
     <div key={movie.id} className="movie-card">
       <img
@@ -16,7 +22,9 @@ export default function MovieCard({ movie }) {
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
-        <p className="movie-card-rating">{movie.rating}</p>
+        <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+          {movie.rating}
+        </p>
       </div>
     </div>
   );
