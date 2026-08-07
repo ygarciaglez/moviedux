e4f9f0a1f7d525fe3ceba9e89bbf3bef91c10b02
ygarciaglez <ygarciaglez1995@gmail.com>
2026-08-07 13:12:39 -0600

    chore(router): configure the Router for the App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
