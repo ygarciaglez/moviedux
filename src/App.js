367e7dcc04955494664cef46d8ca2f493f33189b
ygarciaglez <ygarciaglez1995@gmail.com>
2026-08-07 12:52:51 -0600

    chore(deps): add react route dom package

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css'
-import Header from './components/Header'
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <Header/>
-        <MoviesGrid/>
+      <div className="container">
+        <Header />
+        <MoviesGrid />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
